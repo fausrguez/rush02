18c054d7f46bc1ee3d9526452c58ebdbb07c5d38
Emanuela Licameli <elicamel@f1r5s7.codam.nl>
2020-08-30 21:57:07 +0200

    trimmed input modified; now num and value

---
+++ Trim_input.c
@@ -0,0 +1,43 @@
+#include "headers.h"
+#include "str_length.h"
+
+
+char *trimmed_input(char *n_record, char *entry_value)
+
+char trimmed_input;
+char numkey;
+char value
+
+int i;
+
+i = 0;
+j = 0;
+l = str_length(str2);
+while (n_record[i] != '\0' && != ':')
+{
+	if (n_record[i] == ' ')
+	{
+		i++;
+	}
+	return (0);
+}
+numkey = &n_record[];
+while (entry_value[j] != '\0';
+{
+	if ((entry_value[j] == ' ')
+	{
+		j++,
+		else
+		{
+			entry_value[l] - 1
+			while (entry_value[l] == ' ')
+			{
+				l--;
+			}
+		}
+	}
+}
+value = &entry_value[];
+
+return (0;
+}
