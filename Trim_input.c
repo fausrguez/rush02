d018f97d1693e702320fcbd0b83f6d7fe4d68c8c
Emanuela Licameli <elicamel@f1r5s7.codam.nl>
2020-08-30 22:10:48 +0200

    trim function complete

--- Trim_input.c
+++ Trim_input.c
@@ -1,13 +1,10 @@
 #include "headers.h"
 #include "str_length.h"
 
-
 char *trimmed_input(char *n_record, char *entry_value)
-
-char trimmed_input;
-char numkey;
+{
+char *numkey;
 char value
-
 int i;
 
 i = 0;
@@ -19,9 +16,11 @@ while (n_record[i] != '\0' && != ':')
 	{
 		i++;
 	}
-	return (0);
 }
 numkey = &n_record[];
+
+return (numkey);
+}
 while (entry_value[j] != '\0';
 {
 	if ((entry_value[j] == ' ')
@@ -39,5 +38,5 @@ while (entry_value[j] != '\0';
 }
 value = &entry_value[];
 
-return (0;
+return (value);
 }
